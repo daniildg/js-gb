928f11bc9b0a5470c348e38a969854a3386ba1fe
Daniil Grishin <ddg071997@gmail.com>
2023-07-02 21:03:22 +0300

    hw5 - added

---
+++ homework/hw5/1.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+Дан объект numbers. 
+Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
+*/
+
+const numbers = {
+  key1: 12,
+  key2: 2,
+  key3: 4,
+  key4: 1,
+  key5: -244,
+  key6: 0,
+  key7: 7,
+};
+
+const sortedNumbers = [];
+for (const key in numbers) {
+  if (numbers[key] >= 3) sortedNumbers.push(numbers[key]);
+}
+console.log(sortedNumbers);
